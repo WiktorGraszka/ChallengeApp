ed509bf606fcbe99e9279f0aaa8588b7787a093f
WiktorGraszka <wgraszka91@gmail.com>
2024-01-03 22:28:11 +0100

    6 lekcja moduł

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,67 +1,20 @@
-﻿int number = 4592;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToArray();
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+﻿
+using ChallengeApp;
 
-foreach (char letter in letters)
-{
-    if(letter == '0')
-    {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++; 
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
-    }
-}
-Console.WriteLine("0->" + counter0);
-Console.WriteLine("1->" + counter1);
-Console.WriteLine("2->" + counter2);
-Console.WriteLine("3->" + counter3);
-Console.WriteLine("4->" + counter4);
-Console.WriteLine("5->" + counter5);
-Console.WriteLine("6->" + counter6);
-Console.WriteLine("7->" + counter7);
-Console.WriteLine("8->" + counter8);
-Console.WriteLine("9->" + counter9);
+User user1 = new User("Wiktor", "789wasd789");
+User user2 = new User("Kinga", "789wasd789");
+User user3 = new User("Mia", "789wasd789");
+User user4 = new User("Jacek", "789wasd789");
+
+
+user1.AddScore(5);
+user1.AddScore(2);
+var result = user1.Result;
+Console.WriteLine(result);
+
+
+
+
+
+
+
